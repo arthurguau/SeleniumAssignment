e15872b0a3484fc58dffd6cb2a9f3fb169f29347
funny grillo <gusongqing@hotmail.com>
2023-05-27 21:28:12 +1000

    change name

--- src/test/java/com/planit/myapp/WebDriverBase.java
+++ src/test/java/com/planit/myapp/WebDriverBase.java
@@ -12,33 +12,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebDriverBase {
 	
 	protected static WebDriver driver;
-	protected static String planitUrl = "https://jupiter.cloud.planittesting.com/";
+	protected static String planitURL = "https://jupiter.cloud.planittesting.com/";
 	
 	//local browser: mvn clean test -Dwebdriver.chrome.driver="C:\\Opt\\Selenium\\chromedriver.exe"
 	//protected static String ChromeDriver = "C:\\Opt\\Selenium\\chromedriver.exe";
 	
 	@BeforeAll
 	static void setup(){
-		System.out.println("@BeforeAll executed");
+		System.out.println("@BeforeAll");
 	    driver = new ChromeDriver();		
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	    driver.get(planitUrl);
+	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	    driver.get(planitURL);
 	}
 
 	@BeforeEach
 	void setupThis(){
-		System.out.println("@BeforeEach executed");
+		System.out.println("@BeforeEach");
 	}
 
     @AfterEach
 	void tearThis(){
 	    driver.manage().deleteAllCookies();
-    	System.out.println("@AfterEach executed");
+    	System.out.println("@AfterEach");
 	}
 
 	@AfterAll
 	static void tear(){
-		System.out.println("@AfterAll executed");
+		System.out.println("@AfterAll");
 	    driver.close();
 	}
 }
